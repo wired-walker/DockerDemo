bd2a0888695c5dc25677ca53ac5a9edfbdabb7bb
Brandon Walker <bwalker465@gmail.com>
2022-04-16 20:18:39 -0400

    Add project files.

---
+++ WebApp/Controllers/RedisController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RedisController : Controller
+    {
+        [HttpGet]
+        public RedisResult GetFoo()
+        {
+
+            var mgr = new RedisManagerPool("DESKTOP-UE0LGGV:6379");
+
+            var fooValue = string.Empty;
+            using (var client = mgr.GetClient())
+            {
+                client.Set("foo", "bar");
+                fooValue = client.Get<string>("foo");
+
+            }
+            return new RedisResult() { Key = "foo", Value = fooValue };
+        }
+    }
+
+    public class RedisResult
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}

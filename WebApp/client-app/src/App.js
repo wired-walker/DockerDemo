37155481ae26011a574f93ee5200fa86542533a8
Brandon Walker <bwalker465@gmail.com>
2022-09-15 19:34:15 -0400

    test

--- WebApp/client-app/src/App.js
+++ WebApp/client-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. MASTER BRANCH - CHANGE 3
+          Edit <code>src/App.js</code> and save to reload. MASTER BRANCH - CHANGE 4
         </p>
         <a
           className="App-link"
